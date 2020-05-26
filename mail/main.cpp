b285f04d76f339965fab425cd240a25833e0efb1
Lê Thành Trung <63889982+letrung97@users.noreply.github.com>
2020-05-26 13:09:26 +0700

    ver 1.5

--- mail/main.cpp
+++ mail/main.cpp
@@ -20,12 +20,12 @@ bool Email_check(string email)
     const regex pattern("(\\w+)(\\.|_)?(\\w*)@(\\w+)(\\.(\\w+))+");
     if (regex_match(email, pattern))
         cout << "\nYour Email-Id is valid, you can continue\n";
-    else 
+    else
         cout << "\nYour Email-Id is invalid, please correct it\n";
     return regex_match(email, pattern);
 }
 
-void Send_mail(string mail,string msg,string to,string title,string pwd) {
+void Send_mail(string mail, string msg, string to, string title, string pwd) {
     ::CoInitialize(NULL);
     IMailPtr oSmtp = NULL;
     oSmtp.CreateInstance(__uuidof(EASendMailObjLib::Mail));
@@ -46,7 +46,6 @@ void Send_mail(string mail,string msg,string to,string title,string pwd) {
     // require User authentication, please remove the following codes.
     oSmtp->UserName = mail.c_str();
     oSmtp->Password = pwd.c_str();
-
     // Most mordern SMTP servers require SSL/TLS connection now.
     // ConnectTryTLS means if server supports SSL/TLS, SSL/TLS will be used automatically.
     oSmtp->ConnectType = ConnectTryTLS;
@@ -71,49 +70,74 @@ void Send_mail(string mail,string msg,string to,string title,string pwd) {
     }
 }
 
+class email {
+private:
+    string mail;
+    string pwd;
+    string title;
+    string msg;
+    list <string> adds;
+public:
+    void add_umail() {
+        cout << "\nplease enter your email\n";
+        do
+            cin >> mail;
+        while (!Email_check(mail));
+        cout << "\nenter your password\n";
+        cin >> pwd;
+    }
+    void add_rmail() {
+        int k = 0;
+        string add;
+        cout << "\nPlease type whose email you want to send\n" << "how many users you want to share this message\n";
+        cin >> k;
+        for (int i = 0; i < k; i++) {
+            do
+                cin >> add;
+            while (!Email_check(add));
+            adds.push_back(add);
+        }
+    }
+    void add_msg() {
+        int k = 0;
+        int check;
+        cout << "\nTitle or subject of your mail please\n";
+        cin.ignore();
+        getline(cin, title);
+        do {
+            cout << "\nyour message ? (just double Enter to end the message)\n";
+            string s;
+            k = 0;
+            do {
+                getline(cin, s);
+                msg += s + "\n";
+                if (s.length() == 0)
+                    k++;
+                else
+                    k = 0;
+            } while (s.length() > 0 && k < 2);
+            cout << "Do you want to edit your message\n" << "Yes(1)/No(0)\n";
+            do
+                cin >> check;
+            while (check > 1);
+        } while (check == 1);
+    }
+    void start_compose() {
+        add_umail();
+        add_rmail();
+        add_msg();
+    }
+    void start_send() {
+        list <string> ::iterator it;
+        for (it = adds.begin(); it != adds.end(); ++it)
+            Send_mail(mail, msg, *it, title, pwd);
+    }
+};
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-
-    string mail, pwd, title, msg;
-    int k = 0;
-    list <string> adds;
-    string add;
-    cout << "\nplease enter your email\n";
-    do
-        cin >> mail;
-    while (!Email_check(mail));
-
-    cout << "\nenter your password\n";
-    cin >> pwd;
-
-    cout << "\nPlease type whose email you want to send\n" << "how many users you want to share this message\n";
-    cin >> k;
-    for (int i = 0; i < k; i++) {
-        do
-            cin >> add;
-        while (!Email_check(add));
-        adds.push_back(add);
-    }
-
-    cout << "\nTitle or subject of your mail please\n";
-    cin.ignore();
-    getline(cin,title);
-
-    cout << "\nyour message ? (just double Enter to end the message)\n";
-    string s;
-    k=0;
-    do {
-        getline(cin, s);
-        msg += s + "\n";
-        if (s.length() == 0)
-            k++;
-        else
-            k = 0;
-    } while (s.length() > 0 && k < 2);
-
-    list <string> ::iterator it;
-    for (it = adds.begin(); it != adds.end(); ++it)
-        Send_mail(mail, msg, *it, title, pwd);
-
+    email mail;
+    mail.start_compose();
+    mail.start_send();
     return 0;
 }
